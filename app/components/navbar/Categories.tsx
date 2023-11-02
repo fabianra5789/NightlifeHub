4b1287f1892b264a1a3a4c88e2b0abe451ddc6c7
Eric Ramirez <fabianra5789@gmail.com>
2023-11-02 12:03:30 -0500

    ER: ICONS AND TRANSLATE.

--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -1,109 +1,68 @@
-'use client';
+"use client";
 
-import { usePathname, useSearchParams } from 'next/navigation';
-import { TbBeach, TbMountain, TbPool } from 'react-icons/tb';
-import { 
-  GiBarn, 
-  GiBoatFishing, 
-  GiCactus, 
-  GiCastle, 
-  GiCaveEntrance, 
-  GiForestCamp, 
+import { usePathname, useSearchParams } from "next/navigation";
+import { TbBeach, TbMountain, TbPool } from "react-icons/tb";
+import {
+  GiBarn,
+  GiBoatFishing,
+  GiCactus,
+  GiCastle,
+  GiCaveEntrance,
+  GiForestCamp,
   GiIsland,
-  GiWindmill
-} from 'react-icons/gi';
-import { FaSkiing } from 'react-icons/fa';
-import { BsSnow } from 'react-icons/bs';
-import { IoDiamond } from 'react-icons/io5';
-import { MdOutlineVilla } from 'react-icons/md';
+  GiWindmill,
+} from "react-icons/gi";
+import { FaSkiing } from "react-icons/fa";
+import { BsSnow } from "react-icons/bs";
+import { IoDiamond } from "react-icons/io5";
+import { MdOutlineVilla } from "react-icons/md";
 
 import CategoryBox from "../CategoryBox";
-import Container from '../Container';
-
+import Container from "../Container";
 
 export const categories = [
   {
-    label: 'Beach',
+    label: "Techno",
     icon: TbBeach,
-    description: 'This property is close to the beach!',
+    description: "Discotecas donde solo ponen Techno!",
   },
   {
-    label: 'Windmills',
+    label: "Salsa",
     icon: GiWindmill,
-    description: 'This property is has windmills!',
+    description: "Discotecas donde solo ponen Salsa!",
   },
   {
-    label: 'Modern',
+    label: "Dancehall",
     icon: MdOutlineVilla,
-    description: 'This property is modern!'
+    description: "Discotecas donde solo ponen Dancehall!",
   },
   {
-    label: 'Countryside',
+    label: "Crossover",
     icon: TbMountain,
-    description: 'This property is in the countryside!'
+    description: "Discotecas donde solo ponen Crossover!",
   },
   {
-    label: 'Pools',
+    label: "Reggaetón",
     icon: TbPool,
-    description: 'This is property has a beautiful pool!'
+    description: "Discotecas donde solo ponen Reggaetón!",
   },
   {
-    label: 'Islands',
-    icon: GiIsland,
-    description: 'This property is on an island!'
-  },
-  {
-    label: 'Lake',
-    icon: GiBoatFishing,
-    description: 'This property is near a lake!'
-  },
-  {
-    label: 'Skiing',
-    icon: FaSkiing,
-    description: 'This property has skiing activies!'
-  },
-  {
-    label: 'Castles',
-    icon: GiCastle,
-    description: 'This property is an ancient castle!'
-  },
-  {
-    label: 'Caves',
-    icon: GiCaveEntrance,
-    description: 'This property is in a spooky cave!'
-  },
-  {
-    label: 'Camping',
-    icon: GiForestCamp,
-    description: 'This property offers camping activities!'
-  },
-  {
-    label: 'Arctic',
-    icon: BsSnow,
-    description: 'This property is in arctic environment!'
-  },
-  {
-    label: 'Desert',
-    icon: GiCactus,
-    description: 'This property is in the desert!'
-  },
-  {
-    label: 'Barns',
-    icon: GiBarn,
-    description: 'This property is in a barn!'
-  },
-  {
-    label: 'Lux',
+    label: "Lujosos",
     icon: IoDiamond,
-    description: 'This property is brand new and luxurious!'
-  }
-]
+    description: "Discotecas Lujosas!",
+  },
+  {
+    label: "Otro",
+    icon: GiBoatFishing,
+    description: "Otro tipo de discotecas!",
+  },
+];
 
 const Categories = () => {
   const params = useSearchParams();
-  const category = params?.get('category');
+  const category = params?.get("category");
   const pathname = usePathname();
-  const isMainPage = pathname === '/';
+  const isMainPage = pathname === "/";
 
   if (!isMainPage) {
     return null;
@@ -122,7 +81,7 @@ const Categories = () => {
         "
       >
         {categories.map((item) => (
-          <CategoryBox 
+          <CategoryBox
             key={item.label}
             label={item.label}
             icon={item.icon}
@@ -132,6 +91,6 @@ const Categories = () => {
       </div>
     </Container>
   );
-}
- 
+};
+
 export default Categories;
